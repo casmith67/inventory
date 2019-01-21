65937e694a5a9b0b5f568c3ff7ed60851f1183ce
Caleb <casmith67@gmail.com>
2019-01-20 23:21:07 -0600

    Updated some views

--- models/login.js
+++ models/login.js
@@ -1,13 +1,16 @@
 const conn = require("./utilities");
 const bcrypt = require("bcryptjs");
-const path = require("path");
 
-function verifyCredentials(res, req, passToCompare) {
+function verifyCredentials(res, req, passToCompare, age) {
   bcrypt.compare(req.body.password, passToCompare, function(err, success) {
     if (err) throw err;
     if (success) {
       req.session.loggedIn = true;
-      res.send("You have successfully logged in");
+      res.render("index", {
+        loggedIn: req.session.loggedIn,
+        username: req.body.username,
+        age: age
+      });
     } else {
       res.send("Invalid password");
     }
@@ -16,11 +19,11 @@ function verifyCredentials(res, req, passToCompare) {
 
 module.exports.authenticateUser = function(req, res) {
   let username = req.body.username;
-  let sql = `SELECT password FROM users WHERE username='${username}'`;
+  let sql = `SELECT password, userAge FROM users WHERE username='${username}'`;
   conn.query(sql, function(err, result) {
     if (err) console.log(err);
     if (result != "") {
-      verifyCredentials(res, req, result[0].password);
+      verifyCredentials(res, req, result[0].password, result[0].userAge);
     } else {
       res.send("You don't have an account with us!");
     }
--- models/register.js
+++ models/register.js
@@ -1,28 +1,31 @@
 const conn = require("./utilities");
 const bcrypt = require("bcryptjs");
 
-function addUser(res, username, hash) {
-  let sql = `INSERT INTO users(username, password) VALUES('${username}', '${hash}')`;
+function addUser(res, username, hash, age) {
+  let sql = `INSERT INTO users(username, password, userAge) VALUES('${username}', '${hash}', '${age}')`;
   conn.query(sql, function(err, result) {
     if (err) throw err;
-    res.send("Thank you for signing up!");
+    res.render("index", {
+      username: username
+    });
   });
 }
 
-function hashPassword(res, username, password) {
+function hashPassword(res, username, password, age) {
   bcrypt.hash(password, 10, function(err, hash) {
     if (err) throw err;
-    addUser(res, username, hash);
+    addUser(res, username, hash, age);
   });
 }
 
 module.exports.CheckForDuplicates = function(req, res) {
   let username = req.body.username;
   let password = req.body.password;
+  let age = req.body.age;
   let sql = `SELECT username FROM users WHERE username='${username}'`;
   conn.query(sql, function(err, result) {
     if (result == "") {
-      hashPassword(res, username, password);
+      hashPassword(res, username, password, age);
     } else {
       res.send("You currently have an account with us!");
     }
